9f01ad2d45aeb2f15620765ea759c9ed511c64f4
Krzysztof Romanowski <romanowski.kr@gmail.com>
2021-10-15 11:24:18 +0200

    Translate into a single file

--- main.scala
+++ main.scala
@@ -8,16 +8,15 @@
 // using tasty-query::tasty-query:0.0.2-spark
 
 // using org.scala-lang::scala3-tasty-inspector:3.0.2
-// using "io.get-coursier:coursier_2.13:2.0.16-169-g194ebc55c"
 // using "org.scala-sbt:io_2.13:1.5.1"
 // using com.lihaoyi::requests:0.6.9
 // using com.lihaoyi::os-lib:0.7.8
 
-
 import scala3encoders.given 
+import scala3encoders.derivation.Serializer
+import scala3encoders.derivation.Deserializer
 
 import org.apache.spark.sql.SparkSession
-import coursier._
 import sbt.io._
 import sbt.io.syntax._
 import java.io.File
@@ -27,11 +26,13 @@ import tastyquery.api.ProjectReader
 import tastyquery.ast.Trees.*
 import tastyquery.ast.Types.*
 import tastyquery.ast.Names.Name
+import java.util.Base64
 
 case class Library(org: String, name: String, version: String)
-case class TastyFile(lib: Library, path: String)(val content: Array[Byte])
+case class TastyFile(lib: Library, path: String, val contentBase64: String)
 case class TreeInfo(lib: Library, sourceFile: String, 
   method: String, kind: String, index: Int, depth: Int, topLevelType: Option[String])
+
 
 def loadTastyFiles(lib: Library): Seq[TastyFile] = 
   // TODO do it in memory!
@@ -50,100 +51,74 @@ def loadTastyFiles(lib: Library): Seq[TastyFile] =
     val tastyFiles = (tastyDir.wrapped.toFile ** "*").filter( ! _.isDirectory).get
     tastyFiles.toList.map { file =>
       val relPath = tastyDir.wrapped.relativize(file.toPath).toString
-      TastyFile(lib, relPath)(os.read.bytes(tastyDir / os.RelPath(relPath)))
+      val bytes = os.read.bytes(tastyDir / os.RelPath(relPath))
+      TastyFile(lib, relPath, Base64.getEncoder().encodeToString(bytes))
     }
   finally os.remove.all(tmpDir)
 
 
-def fetch(tempDir: File)(org: String, name: String, version: String, prefix: String): LibraryWrapper = 
-  import coursier.*
-  val params = coursier.params.ResolutionParams().withScalaVersion("3.0.2")
-  val dep = Dependency(Module(Organization(org), ModuleName(name)), version)
-  val result = Fetch().addDependencies(dep).withResolutionParams(params).runResult()
-  val jars = result.detailedArtifacts.filter(_._2.`type` == Type.jar)
-  val aDep = jars.filter { case (dep, _, _, _) => 
-    dep.module.name.value.startsWith(prefix) && dep.module.organization.value == org  
-  }
+def printType(tpe: Type): String = tpe match
+   case PackageRef(pck) => pck.toString
+   case TypeRef(base, name) => printType(base) + "." + name.toString
+   case ThisType(base) => printType(base) + ".this"
+   case TermRef(base,name) => printType(base) + "." + name.toString
+   case AppliedType(base, args) => printType(base) + args.map(_ => "_").mkString("[", ", ", "]")
+   case _ => "TODO:" + tpe.toString
 
-  val dest = tempDir / s"$org-$name-$version"
-  IO.createDirectory(dest) 
-
-  aDep.foreach(d => IO.unzip(d._4, dest, ExtensionFilter("tasty")))
-  val tastyFiles = (dest ** "*").filter( ! _.isDirectory).get.map(_.toString)
-
-  // Tasty reader is broken and we need to provide both .class files and .tasty files now
-  LibraryWrapper(
-    org,
-    name,
-    version,
-    tastyFiles.mkString(File.pathSeparator), // TODO move resolution locally to node
-    jars.map(_._4.toString).mkString(File.pathSeparator)
-  )
+def isInteresting(tpe: Type): Boolean = tpe match
+  case _: PackageRef => false
+  case _ => true
 
 def processTastyFile(tastyFile: TastyFile): Seq[TreeInfo] = 
-  val unpickler = new TastyUnpickler(tastyFile.content)
-  val ast = 
-    unpickler.unpickle(new TastyUnpickler.TreeSectionUnpickler()).get.unpickle(using Contexts.empty(tastyFile.path))
+  try
+    val content =  Base64.getDecoder().decode(tastyFile.contentBase64)
+    val unpickler = new TastyUnpickler(content)
+    val ast = 
+      unpickler.unpickle(new TastyUnpickler.TreeSectionUnpickler()).get.unpickle(using Contexts.empty(tastyFile.path))
 
-  def walk(t: Tree, index: Int, depth: Int, method: String, topLevel: Boolean): (Int, Seq[TreeInfo]) =
-    def processChildren(
-      newMethod: String = method,
-      nIndex: Int = index, 
-      trees: Seq[TreeInfo] = Nil, 
-      newTopLevel: Boolean = topLevel
-      ) =
-        t.subtrees.foldLeft((nIndex, trees)){
-          case ((index, trees), tree) =>
-            val (nIndex, newTrees) = walk(tree, index, depth + 1, newMethod, topLevel)
-            (nIndex, trees ++ newTrees)
-        }
+    def walk(t: Tree, index: Int, depth: Int, method: String, topLevel: Boolean): (Int, Seq[TreeInfo]) =
+      def processChildren(
+        newMethod: String = method,
+        nIndex: Int = index, 
+        trees: Seq[TreeInfo] = Nil, 
+        newTopLevel: Boolean = topLevel
+        ) =
+          t.subtrees.foldLeft((nIndex, trees)){
+            case ((index, trees), tree) =>
+              val (nIndex, newTrees) = walk(tree, index, depth + 1, newMethod, topLevel)
+              (nIndex, trees ++ newTrees)
+          }
 
-    def processTreeAndChildren(newMethod: String = method) =
-      val newIndex = index + 1
-      val tpe = t.tpeOpt.map {
-        case named: NamedType => 
-          named.name.toString + ":" + named.toString
-        case other =>
-          other.toString
-      }
-      val info = TreeInfo(
-        tastyFile.lib, tastyFile.path, newMethod, t.getClass.getSimpleName,
-        depth, newIndex, tpe
-      )
-      processChildren(newMethod, index + 1, Seq(info))
+      def processTreeAndChildren(newMethod: String = method) =
+        val newIndex = index + 1
+        val info = TreeInfo(
+          tastyFile.lib, tastyFile.path, newMethod, t.getClass.getSimpleName,
+          depth, newIndex, t.tpeOpt.filter(isInteresting).map(printType)
+        )
+        processChildren(newMethod, index + 1, Seq(info))
 
-    def ignore = (index, Nil)
+      def ignore = (index, Nil)
 
-    def newMethod(name: Name) = 
-      if method.isEmpty then name.toString else s"$method.$name"
+      def newMethod(name: Name) = 
+        if method.isEmpty then name.toString else s"$method.$name"
 
-    t match
-      case PackageDef(pid, stats)                   => processChildren(newMethod(pid.name))
-      case ImportSelector(imported, renamed, bound) => ignore
-      case Import(expr, selectors)                  => ignore
-      case Class(name, rhs, symbol)                 => processChildren(newMethod(name))
-      case _: Template | _: Block                   => processChildren()
-      case ValDef(name, tpt, rhs, symbol)           => processChildren(newMethod(name), newTopLevel = false)
-      case DefDef(name, params, tpt, rhs, symbol)   => processChildren(newMethod(name), newTopLevel = false)
-      case _                                        => processTreeAndChildren()
+      t match
+        case PackageDef(pid, stats)                   => processChildren(newMethod(pid.name))
+        case ImportSelector(imported, renamed, bound) => ignore
+        case Import(expr, selectors)                  => ignore
+        case Class(name, rhs, symbol)                 => processChildren(newMethod(name))
+        case _: Template | _: Block                   => processChildren()
+        case ValDef(name, tpt, rhs, symbol)           => processChildren(newMethod(name), newTopLevel = false)
+        case DefDef(name, params, tpt, rhs, symbol)   => processChildren(newMethod(name), newTopLevel = false)
+        case _                                        => processTreeAndChildren()
 
-  ast.flatMap(walk(_ , 0, 0, "", topLevel = true)._2)
-  
+    ast.flatMap(walk(_ , 0, 0, "", topLevel = true)._2)
+  catch 
+    case e: Throwable => // Ugly AF
+      // e.printStackTrace
+      Nil
 
-@main def tests =
-  val libs = Seq(
-      // Library("org.typelevel", "cats-core", "2.6.1"), // OK
-      // Library("dev.zio", "zio", "2.0.0-M3"), // Fails with missing annotations
-      // Library("org.typelevel", "cats-effect", "3.2.9"), // OK
-      // Library("org.http4s", "http4s-core", "1.0.0-M23"), // Fails with NoSymbol
-      // Library( "co.fs2" , "fs2-core" , "3.1.5"), // undefined: fs2.Stream.bracket # -1
-      Library( "org.tpolecat", "doobie-core" , "0.13.4"), // Fails with undefined: fs2.Stream.Compiler.syncInstance
-      // Library( "io.monix" , "monix-eval" , "3.4.0") // OK
-  )
-  println(libs.map(loadTastyFiles).flatMap(_.take(3)).flatMap(processTastyFile).filter(_.topLevelType.nonEmpty).take(25).mkString("\n"))
-  
-
-def app = 
+@main def app = 
   val baseFile = IO.createTemporaryDirectory
   try {
     lazy val spark: SparkSession = {
@@ -158,23 +133,41 @@ def app =
     import sqlContext.implicits._
     import org.apache.spark.sql.functions._
 
-    val fetchToTemp = fetch(baseFile)
 
-    val librariesDF = List(
-      fetchToTemp("org.typelevel", "cats-core_3", "2.6.1", "cats"), // OK
-      // fetchToTemp("dev.zio", "zio_3", "2.0.0-M3", "zio"), // Fails with missing annotations
-      fetchToTemp("org.typelevel", "cats-effect_3", "3.2.9", "cats-effect"), // OK
-      // fetchToTemp("org.http4s", "http4s-core_3", "1.0.0-M23", "http4s") // Fails with NoSymbol
-      // fetchToTemp( "co.fs2" , "fs2-core_3" , "3.1.5", "fs2"), // undefined: fs2.Stream.bracket # -1
-      // fetchToTemp( "org.tpolecat" , "doobie-core_3" , "0.13.4", "doobie"), // Fails with undefined: fs2.Stream.Compiler.syncInstance
-      fetchToTemp( "io.monix" , "monix-eval_3" , "3.4.0", "monix") // OK
-    ).toDF
+    val libs = Seq(
+        Library("org.typelevel", "cats-core", "2.6.1"),
+        Library("dev.zio", "zio", "2.0.0-M3"), 
+        Library("org.http4s", "http4s-core", "1.0.0-M23"),
+        Library( "co.fs2" , "fs2-core" , "3.1.5"), 
+        Library( "org.tpolecat", "doobie-core" , "0.13.4"),
+        Library( "io.monix" , "monix-eval" , "3.4.0")
+    )
       
-    val usagesDF = librariesDF.as[LibraryWrapper].flatMap(MethodCollector.collect)
+    val usagesDF = libs
+      .toDF
+      .as[Library]
+      .flatMap(loadTastyFiles)
+      //.as[TastyFile]
+      .flatMap(processTastyFile)
+      // .filter(_.topLevelType.exists(_.startsWith("TODO:"))) // does not work with
+            //       Exception in thread main: org.apache.spark.sql.AnalysisException: cannot resolve 'wrapoption(topLevelType.toString, StringType)' due to data type mismatch: argument 1 requires string type, however, 'topLevelType.toString' is of java.lang.String type.;
+            // 'TypedFilter main$package$$$Lambda$1358/0x0000000800c02040@f511a8e, class TreeInfo, [StructField(lib,Seq(StructField(org,StringType,true), StructField(name,StringType,true), StructField(version,StringType,true)),true), StructField(sourceFile,StringType,true), StructField(method,StringType,true), StructField(kind,StringType,true), StructField(index,IntegerType,true), StructField(depth,IntegerType,true), StructField(topLevelType,StringType,true)], newInstance(class TreeInfo)
+            // +- SerializeFromObject [if (isnull(knownnotnull(assertnotnull(input[0, TreeInfo, true])).lib)) null else named_struct(org, staticinvoke(class org.apache.spark.unsafe.types.UTF8String, StringType, fromString, knownnotnull(knownnotnull(assertnotnull(input[0, TreeInfo, true])).lib).org, true, false), name, staticinvoke(class org.apache.spark.unsafe.types.UTF8String, StringType, fromString, knownnotnull(knownnotnull(assertnotnull(input[0, TreeInfo, true])).lib).name, true, false), version, staticinvoke(class org.apache.spark.unsafe.types.UTF8String, StringType, fromString, knownnotnull(knownnotnull(assertnotnull(input[0, TreeInfo, true])).lib).version, true, false)) AS lib#37, staticinvoke(class org.apache.spark.unsafe.types.UTF8String, StringType, fromString, knownnotnull(assertnotnull(input[0, TreeInfo, true])).sourceFile, true, false) AS sourceFile#38, staticinvoke(class org.apache.spark.unsafe.types.UTF8String, StringType, fromString, knownnotnull(assertnotnull(input[0, TreeInfo, true])).method, true, false) AS method#39, staticinvoke(class org.apache.spark.unsafe.types.UTF8String, StringType, fromString, knownnotnull(assertnotnull(input[0, TreeInfo, true])).kind, true, false) AS kind#40, knownnotnull(assertnotnull(input[0, TreeInfo, true])).index AS index#41, knownnotnull(assertnotnull(input[0, TreeInfo, true])).depth AS depth#42, staticinvoke(class org.apache.spark.unsafe.types.UTF8String, StringType, fromString, unwrapoption(ObjectType(class java.lang.String), knownnotnull(assertnotnull(input[0, TreeInfo, true])).topLevelType), true, false) AS topLevelType#43]
+            //    +- MapPartitions org.apache.spark.sql.Dataset$$Lambda$1340/0x0000000800bed840@5e9db5e7, obj#36: TreeInfo
+            //       +- DeserializeToObject newInstance(class TastyFile), obj#35: TastyFile
+            //          +- SerializeFromObject [if (isnull(knownnotnull(assertnotnull(input[0, TastyFile, true])).lib)) null else named_struct(org, staticinvoke(class org.apache.spark.unsafe.types.UTF8String, StringType, fromString, knownnotnull(knownnotnull(assertnotnull(input[0, TastyFile, true])).lib).org, true, false), name, staticinvoke(class org.apache.spark.unsafe.types.UTF8String, StringType, fromString, knownnotnull(knownnotnull(assertnotnull(input[0, TastyFile, true])).lib).name, true, false), version, staticinvoke(class org.apache.spark.unsafe.types.UTF8String, StringType, fromString, knownnotnull(knownnotnull(assertnotnull(input[0, TastyFile, true])).lib).version, true, false)) AS lib#25, staticinvoke(class org.apache.spark.unsafe.types.UTF8String, StringType, fromString, knownnotnull(assertnotnull(input[0, TastyFile, true])).path, true, false) AS path#26, staticinvoke(class org.apache.spark.unsafe.types.UTF8String, StringType, fromString, knownnotnull(assertnotnull(input[0, TastyFile, true])).contentBase64, true, false) AS contentBase64#27]
+            //             +- MapPartitions org.apache.spark.sql.Dataset$$Lambda$1340/0x0000000800bed840@5fd8302e, obj#24: TastyFile
+            //                +- DeserializeToObject newInstance(class Library), obj#23: Library
+            //                   +- LocalRelation [org#3, name#4, version#5]
 
-    val reduced = usagesDF.groupBy("name").count().sort(col("count").desc)
+    val counts = usagesDF
+      // Exception in thread main: org.apache.spark.sql.AnalysisException: cannot resolve 'wrapoption(topLevelType.toString, StringType)' due to data type mismatch: argument 1 requires string type, however, 'topLevelType.toString' is of java.lang.String type.
+      // .filter(col("topLevelType").startsWith("TODO")) 
+      .groupBy("topLevelType")
+      .count()
+      .sort(col("count").desc)
 
-    reduced.show(100, false)
+    counts.show(1000, false)
 
     spark.stop()
   } finally IO.delete(baseFile)
--- model.scala
+++
@@ -1,4 +0,0 @@
-case class MethodUsage(name: String)
-
-case class LibraryWrapper(org: String, name: String, version: String, tastyCp: String, classpath: String):
-  def id = s"$org:$name:$version"
